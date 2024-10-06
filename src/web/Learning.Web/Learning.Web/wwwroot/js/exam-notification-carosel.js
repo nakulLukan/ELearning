4746e64d51b94286130414da6e95d7c1a9289422
nakul <matrixlukan@gmail.com>
2024-10-06 13:41:29 +0530

    Design update: Exam notification in homepage

---
+++ src/web/Learning.Web/Learning.Web/wwwroot/js/exam-notification-carosel.js
@@ -0,0 +1,17 @@
+﻿window.initCarousel = function (dotNetHelper) {
+    var carousel = document.getElementById('examNotificationCarousel');
+
+    // Set the initial href
+    var activeItem = carousel.querySelector('.carousel-item.active');
+    var initialHref = activeItem.getAttribute('data-details-href');
+    dotNetHelper.invokeMethodAsync('UpdateHref', initialHref);
+
+    // Listen for the carousel's slide event
+    $('#examNotificationCarousel').on('slide.bs.carousel', function (event) {
+        var newActiveItem = event.relatedTarget;
+        var newHref = newActiveItem.getAttribute('data-details-href');
+
+        // Call Blazor method to update the href
+        dotNetHelper.invokeMethodAsync('UpdateHref', newHref);
+    });
+}
