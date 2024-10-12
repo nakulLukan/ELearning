0be627408476504271806d311a6f92e486193564
nakul <matrixlukan@gmail.com>
2024-10-12 19:41:19 +0530

    Home design figma completed

---
+++ src/web/Learning.Web/Learning.Web.Client/Constants/GlyphIcons.cs
@@ -0,0 +1,8 @@
+﻿namespace Learning.Web.Client.Constants;
+
+public class GlyphIcons
+{
+    public const string Mail = "/assets/images/icons/mail.svg";
+    public const string Whatsapp = "/assets/images/icons/whatsapp.svg";
+    public const string PhoneDialer = "/assets/images/icons/phone_dialer.svg";
+}
