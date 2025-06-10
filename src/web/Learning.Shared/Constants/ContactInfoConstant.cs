dfcf2488c3077d762f1ce853b919bd09e42c8f54
nakul <matrixlukan@gmail.com>
2025-06-10 21:12:37 +0530

    Contact Us | Admin Bug fixes

--- src/web/Learning.Shared/Constants/ContactInfoConstant.cs
+++ src/web/Learning.Shared/Constants/ContactInfoConstant.cs
@@ -4,10 +4,9 @@ public static class ContactInfoConstant
 {
     public const string Whatsapp = "9895007374";
     public const string Mobile1 = "9895007374";
-    public const string Mobile2 = "9895007375";
     public const string EmailAddress = "btsdefenceacademy@gmail.com";
     public const string Instagram = "https://www.instagram.com/bts_defence_academy";
     public const string Facebook = "https://www.facebook.com/profile.php?id=100064131785560";
-    public const string WhatsappChannel = "https://whatsapp.com/channel/[channelId]";
-    public static readonly string[] Branches = ["Perinthalmanna", "Palakkad", "Feroke"];
+    public const string YoutubeChannel = "https://youtube.com/@btsdefenceacademy?si=RPfcAZrgvl6Xsupa";
+    public static readonly string[] Branches = ["Feroke|9895007375", "Perinthalmanna|9656525351", "Palakkad|9746962176", ];
 }
--- src/web/Learning.Web/Learning.Web.Client/Constants/GlyphIcons.cs
+++ src/web/Learning.Web/Learning.Web.Client/Constants/GlyphIcons.cs
@@ -6,6 +6,8 @@ public class GlyphIcons
     public const string MailNeg = "/assets/images/icons/mail_neg.svg";
     public const string Whatsapp = "/assets/images/icons/whatsapp.svg";
     public const string WhatsappNeg = "/assets/images/icons/whatsapp_neg.svg";
+    public const string Youtube = "/assets/images/icons/youtube.svg";
+    public const string YoutubeNeg = "/assets/images/icons/youtube_neg.svg";
     public const string Fb = "/assets/images/icons/fb.svg";
     public const string FbNeg = "/assets/images/icons/fb_neg.svg";
     public const string Insta = "/assets/images/icons/insta.svg";
